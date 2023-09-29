362d15e4ad09910c25de4cd0f6686922b467bcdb
Zahar <91004012+zaharsm@users.noreply.github.com>
2023-09-29 20:05:10 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL : "https://zahar-whatsapp.herokuapp.com",
+    baseURL : "https://whatsapp-backend-three.vercel.app/",
 });
 
 export default instance;
